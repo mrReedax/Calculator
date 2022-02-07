efc579315ecb8d8f1bd639d1b5834f97dc2dfa6d
mrReedax <andresfigueredo560@gmail.com>
2022-02-06 19:38:42 -0500

    div sizes changed, the color of = button was changed

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import './Button.css';
 import React from 'react';
 
 export const Button = ({ content }) => {
-    return (content.map( text =>
+    return (content.map(text =>
         <button key={ text } type="submit" value={text} className="btn">{text}</button>))
 };
 
